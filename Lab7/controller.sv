1b4ea03998cac02abd0c154157859c2b48059387
Yoon Minseo <97289852+bidulki-99@users.noreply.github.com>
2022-12-27 22:23:47 +0900

    Update controller.sv

--- Lab7/controller.sv
+++ Lab7/controller.sv
@@ -5,7 +5,20 @@ module controller(
   output logic [2:0] oALUControl
 );
 
-
-endmodule
- 
+  initial begin
+    oALUControl = 3'b010;
+  end
   
+  always_comb
+    case (iOp)
+      6'b100011: begin
+        oRegWrite = 1;
+        oMemWrite = 0;
+      end
+      default: begin
+        oRegWrite = 0;
+        oMemWrite = 0;
+      end
+    endcase
+  
+endmodule
